c469bc282c702fc8a86b597ed9e67280c3428c41
genjutsu <andresd.ad5@gmail.com>
2023-02-03 00:42:20 -0300

    add component skills and

--- src/components/Cloud/Cloud.tsx
+++ src/components/Cloud/Cloud.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import cloud1 from "../../assets/cloud1.webp"
 import cloud3 from "../../assets/cloud3.webp"
 import cloud4 from "../../assets/cloud4.webp"
@@ -7,13 +7,11 @@ import cloud4 from "../../assets/cloud4.webp"
 export default function Cloud() {
 
   let elementSol:Element
-  const cargarImagen =(entradas:any,obserbador:any)=>{
-    
+  const cargarImagen =(entradas:any,obserbador:any)=>{  
     const n1:any = document.querySelector("#n1")  
     const n2:any = document.querySelector("#n2") 
     const n3:any  = document.querySelector("#n3")
     const n4:any = document.querySelector("#n4") 
-
     if(entradas[0].isIntersecting){
      
           n1.style.transform = "translate(-1800px,-300px)"
@@ -21,8 +19,8 @@ export default function Cloud() {
           n3.style.transform = "translate(30%,-100px)"
           n4.style.transform = "translate(30%,200px)"
           elementSol.classList.add("sol--size")
+          
         }else{
-
           n1.style.removeProperty("transform")
           n2.style.removeProperty("transform")
           n3.style.removeProperty("transform")
@@ -53,7 +51,9 @@ useEffect(()=>{
   
      <div className='cloud'>
         <div className="container">
-          <div className='sol'></div>
+          <div className='sol'>
+          </div> 
+            {/* <img className='luna' src='https://play-lh.googleusercontent.com/13bG-yQS8wU5YKOEqAAC0Xe7N8RE4HNaKQ7-VTOMMaVsBtQM2G3jAXuan7shKRUDQg' alt='luna'/>   */}
           <img id='n1' src={cloud1} loading="lazy" alt='cloud-1'/>
           <img id='n2' src={cloud1} loading="lazy" alt='cloud-2'/>
           <img id='n3' src={cloud3} loading="lazy" alt='cloud-3'/>
