a0f64aec3f07494b8bc2e82acc4b1ae98ae0b05d
genjutsu <andresd.ad5@gmail.com>
2023-02-04 02:13:06 -0300

    add componet projects and contact ant whatsap

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,7 +7,6 @@ export default function Nav() {
             <h1>Welcome Portfolio</h1>
             <div>
              <a href='#'>About</a>
-             <a href='#'>Contact</a>
              <a href='https://github.com/andresDiazLugo' target="_blank">GitHub</a>
              <a href='https://www.linkedin.com/in/alberto-diaz-lugo-91b0a0239/https://www.linkedin.com/in/alberto-diaz-lugo-91b0a0239/' target="_blank">Linkedin</a>
             </div>
---
+++ src/components/Whatsap/Whatsap.tsx
@@ -0,0 +1,8 @@
+import {ImWhatsapp } from 'react-icons/im';
+export default function Whatsap() {
+  return (
+    <div className='whatsap'>
+        <a href='https://api.whatsapp.com/send?phone=5493876116914&text=nos%20gustaria%20concordar%20una%20entrevista%20con%20usted,%20ya%20que%20nos%20intereso%20su%20perfil'target="_blank"><ImWhatsapp color='green' size="4em"/></a>
+    </div>
+  )
+}
